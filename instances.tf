15ed62ff919b2780aada669806440b5692829894
voipdev.sourav <voipdev.sourav@gmail.com>
2019-08-15 22:53:01 +0530

    final commit" "

--- instances.tf
+++ instances.tf
@@ -3,7 +3,9 @@ resource "aws_instance" "dev" {
   instance_type               = "${var.aws_instance_type}"
   key_name                    = "${aws_key_pair.admin_key.key_name}"
   security_groups             = ["${aws_security_group.base_security_group.name}"]
-  associate_public_ip_address = true
+associate_public_ip_address = true
+
+user_data = "${file("conf_apache.sh")}"
 
   tags {
     Name = "Ubuntu16 launched by Devops-Sourav using Terraform"
